2d9ceda3f86adba8f4fdc538d586310dd78a358a
makemeunsee <makemeunsee@gmail.com>
2015-01-13 12:50:07 +0100

    pending bug detailed (numeric approximation problem).

--- Test-PlaneCut.hs
+++ Test-PlaneCut.hs
@@ -19,22 +19,25 @@ main = hspec $ do
                                    ]
       chain unchainedBugSimplified [] `shouldBe` Nothing
 
---      let unchainedBug = [ ((p,[149,23,15]),0)
---                         , ((p,[149,23,15]),1)
---                         , ((p,[149,35,1]),2)
---                         , ((p,[149,60,15]),3)
---                         , ((p,[149,97,60]),4)
---                         , ((p,[149,97,35]),5)
---                         , ((p,[149,129,23]),6)
---                         , ((p,[149,129,1]),7)]
---      chain unchainedBug [] `shouldBe` Nothing
---
---      let unchainedBug' = [ ((Point3f (-0.58830523) (-0.3445781) 0.79916763,[149,23,15]),297)
---                          , ((Point3f (-0.58830523) (-0.3445781) 0.79916763,[149,23,15]),296)
---                          , ((Point3f (-0.20047778) (-0.17686161) 0.9938907,[149,35,1]),295)
---                          , ((Point3f (-0.4753977) (-0.41337168) 0.84328157,[149,60,15]),294)
---                          , ((Point3f (-0.42957) (-0.4192081) 0.86354816,[149,97,60]),293)
---                          , ((Point3f (-0.20466718) (-0.2832958) 0.9806015,[149,97,35]),292)
---                          , ((Point3f (-0.6244454) (-9.031766e-2) 0.80987746,[149,129,23]),291)
---                          , ((Point3f (-0.44437754) (-8.311763e-3) 0.9007293,[149,129,1]),290)]
---      chain unchainedBug' [] `shouldBe` Nothing
+  -- TODO no formal solution yet. work around is to lower tolerance
+  describe "numeric approximation bug" $ do
+    it "should cut" $ do
+      let p0 = (0,(Point3f 0 0.1 0.1,[0,1,4]))
+      let p1 = (1,(Point3f 10 1 1,[0,1,2]))
+      let p2 = (2,(Point3f 10 (-1) (-1),[0,2,3]))
+      let p3 = (3,(Point3f 0 (-0.1) (-0.1),[0,3,4]))
+      let pts = [p0,p1,p2,p3]
+      let f0 = ([0,1,2,3], 0)
+      let f1 = ([0,1], 1)
+      let f2 = ([1,2], 2)
+      let f3 = ([2,3], 3)
+      let f4 = ([0,3], 4)
+      let fs = [f0,f1,f2,f3,f4]
+      let ns = take 5 $ zip (repeat $ normalized $ Point3f 0 1 (-1)) [0..]
+      let fm = FacedModel pts fs ns
+
+      let tolerance = 0.1
+
+      let FacedModel pts' fs' ns' = cutModel tolerance (Plane 0 1 0 $ Point3f 5 0 0) fm
+      pts' `shouldBe` [(2,(Point3f 10.0 (-1.0) (-1.0),[0,2,3])),(3,(Point3f 0.0 (-0.1) (-0.1),[0,3,4])),(5,(Point3f 10.0 0.0 0.0,[5,0,2])),(4,(Point3f 0.0 0.0 0.0,[5,0,4]))]
+      fs' `shouldBe` [([4,5],5),([5,2,3,4],0),([2,5],2),([3,2],3),([3,4],4)]
